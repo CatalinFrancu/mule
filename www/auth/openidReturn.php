bfadf8295255fa808416f8ec68cac610e76b86c1
Cătălin Frâncu <cata@francu.com>
2013-11-22 17:21:16 +0200

    Change wording. Set the default locale to Romanian.

--- www/auth/openidReturn.php
+++ www/auth/openidReturn.php
@@ -9,17 +9,17 @@ if (!$identity) {
 
 $loggedInUser = Session::getUser();
 if ($loggedInUser) {
-  // user is logged in and trying to add a new OpenID to their account
+  // user is logged in and trying to link a new OpenID to their account
   if (!$user->id) {
     // good, we haven't seen this OpenID  before
     $identity->userId = $loggedInUser->id;
     $identity->save();
-    FlashMessage::add(_('New OpenID added to your account.'), 'info');
+    FlashMessage::add(_('New OpenID linked to your account.'), 'info');
   } else if ($user->id == $loggedInUser->id) {
     FlashMessage::add(_('This OpenID is already linked to your account.'), 'warning');
   } else {
     FlashMessage::add(_("This OpenID is already linked to another account. We cannot merge accounts. " .
-                        "Please close the other account before adding the OpenID to this one."));
+                        "Please close the other account before linking the OpenID to this one."));
   }
   Util::redirect('account');
 } else if ($user->id) {
